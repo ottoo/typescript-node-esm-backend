72ceee5a28eaf5db703ec46c49584ea85da8430b
Otto Kivikärki <otto.kivikarki@vincit.com>
2023-06-11 16:02:54 -0700

    Disable playground in prod

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import 'reflect-metadata';
 
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer, ApolloServerPlugin, BaseContext } from '@apollo/server';
+import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { schema } from './schema.js';
 import { DataSource } from 'typeorm';
 import { dataSource } from './config/typeorm/postgres.datasource.js';
 import { Container } from 'typedi';
 
+/**
+ * Set the data source in the container for sharing it with the repositories. This is a workaround for the lack of
+ * support for DI in TypeORM since they deprected the useContainer method for some reason.
+ * https://github.com/typeorm/typeorm/issues/9096
+ */
 Container.set(DataSource, dataSource);
+
+const addPlugins = () => {
+  const plugins: ApolloServerPlugin<BaseContext>[] = [];
+
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(ApolloServerPluginLandingPageDisabled());
+  }
+  return plugins;
+};
 
 const server = new ApolloServer({
   schema,
+  plugins: addPlugins(),
 });
 
 const { url } = await startStandaloneServer(server, {
